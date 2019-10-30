72de7412c0a8dadc493b81ca846b71128ec7811e
Sunman <gksrmf382@naver.com>
2019-10-31 00:23:55 +0900

    on going

--- SWEA/이상한_피라미드_탐험.py
+++ SWEA/이상한_피라미드_탐험.py
@@ -2,18 +2,39 @@ import sys
 import timeit
 import pprint
 
-sys.stdin = open('활주로_건설.txt', 'r')
+sys.stdin = open('이상한_피라미드_탐험', 'r')
 
 start_time = timeit.default_timer()
 
+indexes = []
+
+for index in range(1, 142):
+    indexes.append(index * (index + 1) // 2)
+
+
+def find_coord(target):
+    y = 0
+    x = 0
+    for index in range(141):
+        if target <= indexes[index]:
+            y = index
+            x = index // 2 - (indexes[index] - target)
+            break
+    return x, y
+
+
 for testCase in range(int(input())):
-	start, end = map(int, input().split())
-	print("#{} {}".format(testCase + 1, cnt))
+    start, end = map(int, input().split())
+    start_x, start_y = find_coord(start)
+    end_x, end_y = find_coord(end)
+    go_y = abs(end_y - start_y)
+    go_x = abs(end_x - start_x) + (0 if go_y % 2 else 1)
+    print("#{} {}".format(testCase + 1, max(go_x, go_y)))
 
 end_time = timeit.default_timer()
 
 print('running time: {}'.format(end_time - start_time))
 
-#1 1
-#2 0
-#3 31
+# 1 1
+# 2 0
+# 3 31
