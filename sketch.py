03b06a53c5f17ee11558d3341b1b419c84e0a42b
gosunman <gksrmf382@naver.com>
2019-11-26 18:00:15 +0900

    ..

--- sketch.py
+++ sketch.py
@@ -1,6 +1,13 @@
-def parent(n):
-    while p[n] != n:
-        n = p[n]
-
-
-p = [index for index in range(10)]
+if 0 <= rr < N and 0 <= cc < N and [rr, cc] not in pathh:
+    if arr[rr][cc] < arr[r][c]:
+        DFS(rr, cc, pathh, k)
+    elif k:
+        for kk in range(1, K + 1):
+            if arr[rr][cc] - kk == arr[r][c] - 1:
+                arr[rr][cc] -= kk
+                DFS(rr, cc, pathh, False)
+                arr[rr][cc] += kk
+            else:
+                Max = max(Max, len(pathh))
+    else:
+        Max = max(Max, len(pathh))
