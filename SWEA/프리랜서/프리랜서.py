871613f98dd40fe6a42f604aaf514bf5a9ee3dc8
Sunman <gksrmf382@naver.com>
2019-12-01 21:50:44 +0900

    c++

--- SWEA/프리랜서/프리랜서.py
+++ SWEA/프리랜서/프리랜서.py
@@ -9,20 +9,27 @@ start_time = timeit.default_timer()
 for testCase in range(int(input())):
     qnt_tasks, qnt_days = map(int, input().split())
     info_tasks = []
-    for _ in range(qnt_tasks):
+    for index in range(qnt_tasks):
         d1, d2, w = map(int, input().split())
         info_tasks.append([d1 - 1, d2 - 1, w])
     info_tasks.sort(key=lambda x: (x[1], -x[0]))
-    table = [-1 for _ in range(qnt_days)]
-    last_point = 0
-    for task_index in range(qnt_tasks):
-        d1, d2, w = info_tasks[task_index]
-        option1 = last_point
-        for index in range(d2, d1-1, -1):
-            table[index]
-        option2 =
-    print("#{} {}".format(testCase + 1, info_tasks))
-    pprint.pprint(table)
+    schedule = [info_tasks[0]]
+    last_point = info_tasks[0][2]
+    for task_index in range(1, qnt_tasks):
+        task = info_tasks[task_index]
+        current_index = len(schedule) - 1
+        last_index = current_index
+        current_point = last_point
+        while current_index >= 0 and schedule[current_index][1] >= task[0]:
+            current_point -= schedule[current_index][2]
+            current_index -= 1
+        current_point += task[2]
+        if current_point > last_point:
+            last_point = current_point
+            for index in range(current_index + 1, last_index + 1):
+                schedule.pop()
+            schedule.append(task)
+    print("#{} {}".format(testCase + 1, schedule))
 
 end_time = timeit.default_timer()
 
