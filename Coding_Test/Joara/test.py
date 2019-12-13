803bbc468e088e618f4f0859bbf74a4526b717b9
gosunman <gksrmf382@naver.com>
2019-12-13 23:04:40 +0900

    joara

---
+++ Coding_Test/Joara/test.py
@@ -0,0 +1,35 @@
+import sys
+import timeit
+import pprint
+
+sys.stdin = open('../../input.txt', 'r')
+
+start_time = timeit.default_timer()
+
+
+def solution(N, K):
+    genuine_count = 0
+    temp_index = 0
+    Nsoldiers = N - 2
+    status_soldiers = [0] + [1 for i in range(N - 2)]
+    while Nsoldiers != 2:
+        while genuine_count != K:
+            temp_index += 1
+            temp_index %= N - 1
+            if status_soldiers[temp_index]:
+                genuine_count += 1
+        status_soldiers[temp_index] = 0
+        Nsoldiers -= 1
+        genuine_count = 0
+    print("the answer is")
+    for i in range(N - 1):
+        if status_soldiers[i]:
+            print(i)
+    print("done")
+
+
+solution(41, 3)
+
+end_time = timeit.default_timer()
+
+print('running time: {}'.format(end_time - start_time))
---
+++ Coding_Test/Joara/test2.py
@@ -0,0 +1,44 @@
+import sys
+import timeit
+import pprint
+
+sys.stdin = open('../../input.txt', 'r')
+
+start_time = timeit.default_timer()
+
+
+def combination(depth, limit, store, count):
+    global checklist, m
+    if depth < limit != count:
+        store.append(depth)
+        combination(depth + 1, limit, store, count + 1)
+        store.pop()
+        combination(depth + 1, limit, store, count)
+    else:
+        if limit == count:
+            checklist.append(store)
+
+
+def solution(n, m):
+    # 관계도
+    status_input = [list(map(int, input().split())) for _ in range(n)]
+
+    # 최소 음식 경우의 수가 i 인경우
+    for i in range(1, m + 1):
+        # 확인해봐야 할 조합의 모음
+        checklist = []
+        combination(0, i, [], 0)
+        for onelist in checklist:
+            count = 0
+            status_friends = [1 for _ in range(n)]
+            for food in range(i):
+                for friend in range(n):
+                    if status_input[friend][onelist[i]] and status_friends[friend]:
+                        status_friends[friend] = 0
+                        count += 1
+            if count == n:
+                return True
+
+end_time = timeit.default_timer()
+
+print('running time: {}'.format(end_time - start_time))
