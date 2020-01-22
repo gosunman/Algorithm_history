f6ce8abcf9e65ebc3ee6cbd3f516236d517221eb
gosunman <gksrmf382@naver.com>
2020-01-23 00:26:45 +0900

    차량_정비소_풀이중

--- SWEA/차량_정비소/차량_정비소.py
+++ SWEA/차량_정비소/차량_정비소.py
@@ -14,12 +14,55 @@ for testCase in range(int(input())):
     S_mechanics = list(map(int, input().split()))
     S_customers = list(map(int, input().split()))
     N_complete = 0
+    N_occupied_receptions = 0
+    N_occupied_mechanics = 0
+    reception_available = [[-1, 0] for i in range(N_receptions)] # 종료시간 + 회원번호
+    mechanic_available = [-1 for i in range(N_mechanics)]
+    waiting = [] # 회원번호 + 접수창고 번호
     time = 0
-    while N_complete == N_customer:
+    answer = 0
+    temp_index = 0
+
+    while N_complete != N_customer:
+
+        # 정비창구에서 빼기
+        for idx in range(N_mechanics):
+            if mechanic_available[idx] == time:
+                mechanic_available[idx] = -1
+                N_occupied_mechanics -= 1
+                N_complete += 1
+
+        # 접수창구에서 빼기
+        for idx in range(N_receptions):
+            if reception_available[idx][0] == time:
+                # 회원 번호 + 접수 창구 번호
+                waiting.append([reception_available[idx][1], idx])
+                reception_available[idx] = [-1, 0]
+                N_occupied_receptions -= 1
+
+        # 정비창구에 넣기
+        for idx in range(N_mechanics):
+            if mechanic_available[idx] == -1:
+                if waiting:
+                    index, number = waiting.pop(0)
+                    mechanic_available[idx] = time + S_mechanics[idx]
+                    if number == first_N and idx == second_N:
+                        answer += index
+                else:
+                    break
+
+        # 접수창구에 넣기
+        for idx in range(N_receptions):
+            if reception_available[idx] == [-1, 0]:
+                if temp_index < N_customer:
+                    reception_available[idx] = [time + S_receptions[idx], temp_index + 1]
+                else:
+                    break
         time += 1
 
-        pass
-    print("#{} {}".format(testCase + 1, 0))
+    if answer == 0:
+        answer = -1
+    print("#{} {}".format(testCase + 1, answer))
 
 end_time = timeit.default_timer()
 
