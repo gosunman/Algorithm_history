0e34f36aaa612490f3cb144e019b0660cdc95033
gosunman <gksrmf382@naver.com>
2020-01-25 23:34:32 +0900

    디오판토스 ...

--- SWEA/디오판토스_방정식/디오판토스_방정식.py
+++ SWEA/디오판토스_방정식/디오판토스_방정식.py
@@ -6,14 +6,26 @@ sys.stdin = open('디오판토스_방정식', 'r')
 
 start_time = timeit.default_timer()
 
+
+def get_gcd(n, m):
+    num = max(a, b)
+    divisor = min(a, b)
+    while (num % divisor) != 0:
+        remainder = num % divisor
+        num = divisor
+        divisor = remainder
+    return divisor
+
+
 for testCase in range(int(input())):
+    x = y = 0
     a, b, c = map(int, input().split())
-    x = 1
-    y = c - 
-    print("#{} {}".format(testCase + 1, 0))
+    gcd = get_gcd(a, b)
+    gcm = a * b // gcd
+    print("#{} {} {}".format(testCase + 1, x, y))
 
 end_time = timeit.default_timer()
 
 print('running time: {}'.format(end_time - start_time))
 
-#1 1 -1
+# 1 1 -1
