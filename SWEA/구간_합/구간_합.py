11a84919874651fd3f8259beeda2fb99adf471a0
gosunman <gksrmf382@naver.com>
2020-01-25 23:57:48 +0900

    ...

--- SWEA/구간_합/구간_합.py
+++ SWEA/구간_합/구간_합.py
@@ -6,6 +6,9 @@ sys.stdin = open('구간_합', 'r')
 
 start_time = timeit.default_timer()
 
+def sol(num):
+
+
 for testCase in range(int(input())):
     start, end = map(int, input().split())
     sum_end =
@@ -18,3 +21,8 @@ print('running time: {}'.format(end_time - start_time))
 #1 46
 #2 23
 #3 940
+
+# 13
+# 01 02 03 04 05 06 07 08 09 10 11 12 13
+# 1+2+3 * 2
+#
