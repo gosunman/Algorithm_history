6bdbc7d47b0ec92f8e87c843663324dd99a905e5
gosunman <gksrmf382@naver.com>
2020-01-25 18:57:16 +0900

    옥희의 OK! 부동산

--- SWEA/옥희의_OK!_부동산/옥희의_OK!_부동산.py
+++ SWEA/옥희의_OK!_부동산/옥희의_OK!_부동산.py
@@ -6,22 +6,47 @@ sys.stdin = open('옥희의_OK!_부동산', 'r')
 
 start_time = timeit.default_timer()
 
-for testCase in range(int(input())):
-    N_land, N_money = map(int, input().split())
-    S_land = [0] + list(map(int, input().split()))
-    index = list(range(N_land))
-    temp = index[:]
-    size = 0
-    # 전체 스캔하면서 카운트
-    # 사이즈가 커지면 맨 오른쪽에 있는 놈을 더해서 원래 값 업데이트
-    # 만약 숫자가 초과하면 인덱스에서 제거
-    # 그래서 매번 전체 후보를 다 조사하는 방식이지만, 인덱스 후보가 줄어들면서 시간 단축
-    # 합산하는 과정도 중간 과정을 기억하기 때문에 계산량이 획기적으로 줄어듬
-    # 문제는.. c++로 풀어야 한다는 점
-    while index:
-        for i in index:
-            if S_land[i] + S_land[i+size] <
-    print("#{} {}".format(testCase + 1, 0))
+#include<iostream>
+using namespace std;
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	cin>>T;
+    int N, M;
+    int answer;
+	for(test_case = 1; test_case <= T; ++test_case)
+	{
+        cin >> N >> M;
+        answer = 0;
+        int sample[N];
+        int temp[N];
+        for (int i = 0; i<N; i++){
+            temp[i] = 0;
+        	cin >> sample[i];
+        }
+        for (int i = 0; i<N; i++){
+            int size = 0;
+        	while (true) {
+            	if (i+size < N){
+                	if (temp[i] + sample[i+size] < M){
+                        temp[i] += sample[i+size];
+                        size += 1;
+                    } else if (temp[i] + sample[i+size] == M){
+                        answer += 1;
+                        break;
+                    } else {
+                    break;
+                    }
+                } else {
+                break;
+                }
+            }
+        }
+        cout << "#" << test_case << " " << answer << endl;
+	}
+	return 0;
+}
 
 end_time = timeit.default_timer()
 
