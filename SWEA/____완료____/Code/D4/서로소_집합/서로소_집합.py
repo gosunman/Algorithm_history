7e47cd271d9755e119e98b27263e68f4d02af9cc
gosunman <gksrmf382@naver.com>
2020-01-30 23:21:33 +0900

    서로소_집합

--- SWEA/____완료____/Code/D4/서로소_집합/서로소_집합.py
+++ SWEA/____완료____/Code/D4/서로소_집합/서로소_집합.py
@@ -6,11 +6,15 @@ sys.stdin = open('서로소_집합', 'r')
 
 start_time = timeit.default_timer()
 
+
 def group(node):
     global fathers
+    count = 0
     while fathers[node] != node:
         node = fathers[node]
-    return node
+        count += 1
+    return node, count
+
 
 for testCase in range(int(input())):
     N_groups, M_operators = map(int, input().split())
@@ -21,16 +25,21 @@ for testCase in range(int(input())):
         n1 -= 1
         n2 -= 1
         if operator:
-            if group(n1) == group(n2):
+            if group(n1)[0] == group(n2)[0]:
                 answer += '1'
             else:
                 answer += '0'
         else:
-            fathers[group(n2)] = group(n1)
+            father1, count1 = group(n1)
+            father2, count2 = group(n2)
+            if count1 > count2:
+                fathers[father2] = father1
+            else:
+                fathers[father1] = father2
     print("#{} {}".format(testCase + 1, answer))
 
 end_time = timeit.default_timer()
 
 print('running time: {}'.format(end_time - start_time))
 
-#1 001
+# 1 001
