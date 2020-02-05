8ea082ac208d28fbf7efa1c89f2d4782e79f7e61
gosunman <gksrmf382@naver.com>
2020-02-05 18:03:43 +0900

    ...

--- SWEA/Shortest_Path_Faster/Shortest_Path_Faster.py
+++ SWEA/Shortest_Path_Faster/Shortest_Path_Faster.py
@@ -1,11 +1,44 @@
 import sys
 import timeit
-import pprint
-import heapq
 
 sys.stdin = open('Shortest_Path_Faster', 'r')
 
 start_time = timeit.default_timer()
+
+que = [0] * 300001
+count = 0
+
+
+def en(ele):
+    global count, que
+    count += 1
+    index = count
+    que[count] = ele
+    while index > 1:
+        if que[index][0] < que[index // 2][0]:
+            que[index], que[index // 2] = que[index // 2], que[index]
+            index //= 2
+
+
+def de():
+    global count, que
+    pop_ele = que[1]
+    que[1], que[count] = que[count], que[1]
+    index = 1
+    while index < count:
+        if index * 2 < count:
+            if index * 2 + 1 < count:
+                target = (index * 2) if que[index * 2] <= que[index * 2 + 1] else (index * 2 + 1)
+                if que[index] > que[target]:
+                    que[index], que[target] = que[target], que[index]
+                    index = target
+            else:
+                if que[index] > que[index * 2]:
+                    que[index], que[index * 2] = que[index * 2], que[index]
+                    index *= 2
+    count -= 1
+    return pop_ele
+
 
 for testCase in range(int(input())):
     N_node, N_edge, start, end = map(int, input().split())
@@ -22,10 +55,12 @@ for testCase in range(int(input())):
         else:
             edges[b] = {a: c}
     available = [1 for _ in range(N_node + 1)]
-    bfs = [[start, 0]]
+    count = 0
+    que = []
+    available[start] = 0
+    en([0, start])
     while start != end:
-        start, point = heapq.heappop(bfs)
-        print(start, point)
+        point, start = de()
         for key in edges[start].keys():
             if key == end:
                 print("#{} {}".format(testCase + 1, point + edges[start][key]))
@@ -33,7 +68,7 @@ for testCase in range(int(input())):
                 break
             if available[key]:
                 available[key] = 0
-                heapq.heappush(bfs, (point + edges[start][key], [key, point + edges[start][key]]))
+                en([key, point + edges[start][key]])
 
 end_time = timeit.default_timer()
 
