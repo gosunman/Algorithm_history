9212af61fe87ec1776be1701822a2d8267a103dd
김선만 <gksrmf382@naver.com>
2020-05-20 17:49:34 +0900

    .

--- SWEA/벽돌_꺠기/벽돌_깨기.py
+++ SWEA/벽돌_꺠기/벽돌_깨기.py
@@ -6,55 +6,87 @@ sys.stdin = open('벽돌_깨기', 'r')
 
 start_time = timeit.default_timer()
 
-moves = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
-
-def bomb(line, data):
-    stack = []
-    bomb = 0
-    for Y in range(limit_y):
-        if data[Y][line]:
-            stack = [line, Y]
-            break
+def find_case(index, N, W, history):
+    global cases
+    if index != N:
+        for i in range(W):
+            history.append(i)
+            find_case(index + 1, N, W, history)
+            history.pop()
     else:
-        return bomb
-    while stack:
-        old_x, old_y = stack.pop()
-        distance = data[old_y][old_x]
-        data[old_y][old_x] = -1
-        temp_stack = []
-        while stack:
-            for move in moves:
-                dx, dy = move
-                new_x, new_y = old_x + dx, old_y + dy
-                temp_stack.append([new_x, new_y])
+        cases.append(history[:])
 
 
-def solution(depth, count, temp):
-    global limit, limit_x, limit_y, maximum_bang
-    if depth != limit:
-        for i in range(limit_x):
-            _table = []
-            for j in range(limit_y):
-                _table.append(temp[j][:])
-                cnt = bomb(i, _table)
-                solution(depth + 1, count + cnt, _table)
-    else:
-        if count > maximum_bang:
-            maximum_bang = count
-
-
-for testCase in range(int(input())):
-    limit, limit_x, limit_y = map(int, input().split())
-    table = [list(map(int, input().split())) for _ in range(limit_y)]
-    maximum_bang = 0
+for tc in range(int(input())):
+    N, W, H = map(int, input().split())
+    status = [list(map(int, input().split())) for i in range(H)]
     total = 0
-    for y in range(limit_y):
-        for x in range(limit_x):
-            if table[y][x]:
+    for y in range(H):
+        for x in range(W):
+            if status[y][x]:
                 total += 1
-    solution(0, 0, table)
-    print("#{} {}".format(testCase + 1, total - maximum_bang))
+
+    cases = []
+    find_case(0, N, W, [])
+
+    hit = 0
+    for case in cases:
+        bang = 0
+        copy_table = [status[i][:] for i in range(H)]
+        for x in case:
+            available = [[1 for j in range(W)] for i in range(H)]
+            stack = []
+            for y in range(H):
+                if copy_table[y][x]:
+                    stack.append([x, y])
+                    bang += 1
+                    available[y][x] = 0
+                    break
+            while stack:
+                i, j = stack.pop()
+                for length in range(1, status[j][i]):
+                    if 0 <= i - length:
+                        if available[j][i - length]:
+                            if status[j][i - length]:
+                                stack.append([i - length, j])
+                                bang += 1
+                                available[j][i - length] = 0
+                    if i + length < W:
+                        if available[j][i + length]:
+                            if status[j][i + length]:
+                                stack.append([i + length, j])
+                                bang += 1
+                                available[j][i + length] = 0
+                    if 0 <= j - length:
+                        if available[j - length][i]:
+                            if status[j - length][i]:
+                                stack.append([i, j - length])
+                                bang += 1
+                                available[j - length][i] = 0
+                    if j + length < H:
+                        if available[j + length][i]:
+                            if status[j + length][i]:
+                                stack.append([i, j + length])
+                                bang += 1
+                                available[j + length][i] = 0
+            # 빈자리 정리 작업
+            for i in range(W):
+                found = -1
+                for j in range(H):
+                    if copy_table[j][i]:
+                        fount = j
+                        break
+                count = 0
+                for j in range(H-1, found, -1):
+                    if copy_table[j][i] == 0:
+                        copy_table[j].pop(i)
+                        count += 1
+                copy_table[j] = [0 for k in range(count)] + copy_table[j]
+
+        if bang > hit:
+            hit = bang
+    print("#{} {}".format(tc + 1, total - hit))
 
 end_time = timeit.default_timer()
 
